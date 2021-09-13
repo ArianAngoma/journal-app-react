81508cc268dcd1f5f73610cee2e90077dbd4f4a5
unknown <arian.angoma@tecsup.edu.pe>
2021-09-13 05:03:40 -0500

    Styles on Sidebar

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,7 +1,13 @@
+import {Sidebar} from "./Sidebar";
+
 export const JournalScreen = () => {
     return (
         <div className="journal__main-content">
+            <Sidebar/>
 
+            <main>
+                <h1>Main Content</h1>
+            </main>
         </div>
     )
 }
