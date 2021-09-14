1579916f9d63b849232e9cdab4582a64168c6ebe
unknown <arian.angoma@tecsup.edu.pe>
2021-09-13 23:46:21 -0500

    Styles on NoteScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -4,6 +4,22 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NoteAppBar/>
+
+            <div className="notes__content">
+                <input type="text"
+                       placeholder="Some awesome title"
+                       className="notes__title-input"
+                       autoComplete="off"/>
+
+                <textarea placeholder="What happened today"
+                          className="notes__textarea"/>
+
+                <div className="notes__image">
+                    <img src="https://www.arsys.es/blog/file/uploads/2017/04/NodeJS.jpg" alt="img"/>
+                </div>
+            </div>
+
+
         </div>
     )
 }
