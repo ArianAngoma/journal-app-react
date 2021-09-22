27b159b33aa872d196a47e317a74c54e935e0678
unknown <arian.angoma@tecsup.edu.pe>
2021-09-22 03:27:41 -0500

    Change note active

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,13 +1,22 @@
 import {NoteAppBar} from "./NoteAppBar";
 import {useSelector} from "react-redux";
 import {useForm} from "../../hooks/useForm";
+import {useEffect, useRef} from "react";
 
 export const NoteScreen = () => {
     const {active: note} = useSelector(state => state.notes);
 
-    const [formValues, handleInputChange] = useForm(note);
+    const [formValues, handleInputChange, reset] = useForm(note);
     console.log(formValues);
     const {body, title} = formValues;
+
+    const activeId = useRef(note.id);
+    useEffect(() => {
+        if (note.id !== activeId.current) {
+            reset(note);
+            activeId.current = note.id;
+        }
+    }, [note, reset]);
 
     return (
         <div className="notes__main-content">
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -4,8 +4,8 @@ export const useForm = (initialState = {}) => {
     const [values, setValues] = useState(initialState);
 
     // Funcion para limpiar el formulario
-    const reset = () => {
-        setValues(initialState)
+    const reset = (newFormState = initialState) => {
+        setValues(newFormState)
     }
 
     const handleInputChange = ({target}) => {
