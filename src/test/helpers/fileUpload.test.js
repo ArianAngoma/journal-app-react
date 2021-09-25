b3fffc8d2a24c80ac835f43b4abd7f6cec6b794e
unknown <arian.angoma@tecsup.edu.pe>
2021-09-24 19:30:06 -0500

    Delete img from Cloudinary

--- src/test/helpers/fileUpload.test.js
+++ src/test/helpers/fileUpload.test.js
@@ -1,14 +1,29 @@
+import cloudinary from 'cloudinary';
 import {fileUpload} from "../../helpers/fileUpload";
+
+cloudinary.config({
+    cloud_name: 'ariannode',
+    api_key: '974663459862787',
+    api_secret: 'iZlLP866j7AvId00R1AmcU2NHhY',
+    secure: true
+});
 
 describe('Pruebas en fileUpload', () => {
     test('Debe de cargar un archivo y retornar el URL', async () => {
         const resp = await fetch('https://19yw4b240vb03ws8qm25h366-wpengine.netdna-ssl.com/wp-content/uploads/10-Free-To-Use-CORS-Proxies-1024x768.png');
         const blob = await resp.blob();
 
-        const file = new File([blob], 'img.jpg');
+        const file = new File([blob], 'img.png');
         const url = await fileUpload(file);
 
         expect(typeof url).toBe('string');
+
+        // Borrar imagen por ID
+        const segments = url.split('/');
+        const imgId = segments[segments.length - 1].replace('.png', '');
+        // console.log(imgId);
+
+        await cloudinary.v2.api.delete_resources(imgId);
     });
 
     test('Debe de retornar un error', async () => {
