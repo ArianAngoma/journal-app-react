7c4ca610ce6dc52bc5ebbd516b55d780793b9dca
unknown <arian.angoma@tecsup.edu.pe>
2021-09-27 22:11:13 -0500

    Pruebas en el componente LoginScreen con el Snapshot

---
+++ src/test/components/auth/LoginScreen.test.js
@@ -0,0 +1,38 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import {mount} from "enzyme";
+import {LoginScreen} from "../../../components/auth/LoginScreen";
+import {Provider} from "react-redux";
+import {MemoryRouter} from "react-router-dom";
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {
+    auth: {},
+    ui: {
+        loading: false,
+        msgError: null
+    }
+};
+
+let store = mockStore(initState);
+
+const wrapper = mount(
+    <Provider store={store}>
+        <MemoryRouter>
+            <LoginScreen/>
+        </MemoryRouter>
+    </Provider>
+);
+
+describe('Pruebas en el componente LoginScreen', () => {
+    beforeEach(() => {
+        store = mockStore(initState);
+    });
+
+    test('Debe de mostrarse correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+});
